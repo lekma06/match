517f7c42cb756bccf160b7579f11807b747292aa
lekma06 <malek.bouneb3@gmail.com>
2022-09-27 08:50:59 +0200

    essaie sur git

---
+++ match.js
@@ -0,0 +1,16 @@
+let equipe1 = "Algérie"; 1;
+let equipe2 = "Senegal"; 0;
+let equipe3 = "Maroc"; 2;
+let equipe4 ="Tunisie"; 1;
+let match1 = equipe1 + " - " + equipe2;
+let match2 = equipe3 + " - " + equipe4;
+let resultatMatch1 = "victoire " + equipe1;
+let resultatMatch2 = "victoire " + equipe4;
+
+let resultat = 0;
+if(resultat<52){
+    console.log(true);
+}else{
+    console.log(false)
+};
+
